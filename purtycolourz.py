38ae7cfe14b856a427cfd1bfb794e638f5c68a73
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-06 14:00:15 -0400

    Updated from Brython Server: 9/6/2016 2:00:14 PM

--- purtycolourz.py
+++ purtycolourz.py
@@ -1,3 +1,19 @@
 from ggame import App
 myapp = App()
 myapp.run()
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+Lgreen = Color (0x00FF33, 0.95)
+turqo = Color (0x40E0D0, 0.99)
+Lgreen = Color (0xFF9900, 1)
+black = Color(0x000000, 0.85)
+
+thinline = LineStyle(1, black)
+# A graphics asset that represents a rectangle
+rectangle = RectangleAsset(60, 25, thinline, Lgreen)
+
+Sprite(rectangle)
+
+myapp=App ()
+myapp.run()
