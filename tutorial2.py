c0d6ddfd1cd069946dc4258547428c75bb4c3f56
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-13 14:00:43 -0400

    Updated from Brython Server: 9/13/2016 2:00:43 PM

--- tutorial2.py
+++ tutorial2.py
@@ -9,14 +9,17 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 crimson = Color(0xA02422, 0.97000000000000000000000000000000000000000000000000000000000000000000000001)
 
-thinline = LineStyle(.000001, black)
-noline = Linestyle(0, blue)
+thinline = LineStyle(1, black)
+noline = LineStyle(0.1, blue)
 mycircle = CircleAsset(5, thinline, blue)
 urcircle = CircleAsset(8, noline, crimson)
+arcircle = CircleAsset(6.6, thinline, green)
 xcoordinates = range(100, 600, 10)
 
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+sprites = [Sprite(urcircle, (x, x*0.5 + 200)) for x in xcoordinates]
+sprites = [Sprite(arcircle, (x, x*0.6 + 20)) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
