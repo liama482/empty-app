78750bab61c70b1ba33058950bd3b55093180e7b
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-21 17:52:26 -0400

    Updated from Brython Server: 9/21/2016 5:52:27 PM

--- lists.py
+++ lists.py
@@ -1,1 +1,4 @@
-
+#Some lists of names...
+bros = ['Liam', 'Hugh', 'Teddy', 'Angelo']
+dfam = ['Nana', 'Bobo', 'Jen-Jen', 'Dad']
+mfam = ['Gramma', 'Grandpa', 'Paul', 'Adam', 'Sean', 'Mary Lynn', 'Mom']
